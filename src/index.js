d445923292e171eeda7c153968d0fad6fcdc4319
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-09-16 01:13:53 +0300

    feat: add brackets implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    let stack = [];
+    let openBrackets = bracketsConfig.map((el) => el[0]);
+    let closeBrackets = bracketsConfig.map((el) => el[1]);
+
+    for (let i = 0; i < str.length; i++) {
+        let posClose = closeBrackets.indexOf(str[i]);
+        let posOpen = openBrackets.indexOf(stack[stack.length - 1]);
+
+        if (openBrackets.includes(str[i]) && closeBrackets.includes(str[i])) {
+            if (str[i] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                stack.push(str[i]);
+            }
+        } else {
+            if (openBrackets.includes(str[i])) {
+                stack.push(str[i]);
+            } else if (posClose === posOpen) {
+                stack.pop();
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
